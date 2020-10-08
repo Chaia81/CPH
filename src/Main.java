f65cecd41ac82be7e10ea7edc5c01d15fb7c2ba5
jacob <jacobbayp@gmail.com>
2020-10-08 10:18:16 +0200

    pænt

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import repository.Repository;
+import util.DatabaseConnectionManager;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        DatabaseConnectionManager databaseConnectionManager = new DatabaseConnectionManager();
+        Repository repository = new Repository();
+
+        //repository.createArrivalList();
+        // repository.createDepartureList();
+        // repository.createGateList();
+
+
     }
 }
